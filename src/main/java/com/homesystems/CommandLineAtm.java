16a64ad8581ada7cb810b7001eb7718f23059ed2
yu ka <ykantoni@gmail.com>
2025-04-09 23:20:19 +0300

    Reformatted main function.

--- src/main/java/com/homesystems/CommandLineAtm.java
+++ src/main/java/com/homesystems/CommandLineAtm.java
@@ -6,10 +6,8 @@ import com.homesystems.Command.Status;
   class CommandLineAtm {
     public static void main(String[] args) {
       Scanner scanner = new Scanner(System.in);
-      CommandProcessorFactory commandProcessorFactory =
-          DaggerCommandProcessorFactory.create();
-      CommandProcessor commandProcessor =
-          commandProcessorFactory.commandProcessor();
+      CommandProcessorFactory commandProcessorFactory = DaggerCommandProcessorFactory.create();
+      CommandProcessor commandProcessor = commandProcessorFactory.commandProcessor();
   
       while (scanner.hasNextLine()) {
         Status unused = commandProcessor.process(scanner.nextLine());
